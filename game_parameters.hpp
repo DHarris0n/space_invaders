2aec4713d752c3fa683fa918ea3fa8d76bb2689b
Douglas Harrison <40650822@live.napier.ac.uk>
2025-10-06 17:27:16 +0100

    Delete game_parameters.hpp

--- game_parameters.hpp
+++
@@ -1,9 +0,0 @@
-//game_parameters.hpp
-#pragma once //insure that this header file is included only once and there will no multiple definition of the same thing
-
-struct Parameters {
-    static constexpr int game_width = 800;
-    static constexpr int game_height = 600;
-    static constexpr int sprite_size = 32;
-    static constexpr float time_step = 0.017f;
-};
